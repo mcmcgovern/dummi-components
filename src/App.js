536930b8cac8902e9d9ae1309ffe4e746ab4cf26
mcmcgovern <michaelmcgovern120@gmail.com>
2020-05-05 23:54:15 -0400

    fixed router, rough outline of feed items

--- src/App.js
+++ src/App.js
@@ -5,30 +5,23 @@ import AboutPage from "./components/AboutPage";
 import SignupPage from "./components/SignupPage";
 import CreativePage from "./components/CreativePage";
 import NotFoundPage from "./components/NotFoundPage";
-import {
-  Route,
-  Switch,
-  Redirect,
-  BrowserRouter as Router,
-} from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/about" component={AboutPage} />
-          <Route path="/signup" component={SignupPage} />
-          <Route path="/creative/:slug" component={CreativePage} />
-          <Route path="/creative" component={CreativePage} />
-          <Redirect from="/about-page" to="/about" />
-          <Route component={NotFoundPage} />
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/signup" component={SignupPage} />
+        <Route path="/creative/:slug" component={CreativePage} />
+        <Route path="/creative" component={CreativePage} />
+        <Redirect from="/about-page" to="/about" />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
   );
 }
 
--- src/components/MutAidFeed.js
+++ src/components/MutAidFeed.js
@@ -1,9 +1,34 @@
 import React from "react";
 
-const MutAidFeed = () => {
+const MutAidFeed = (props) => {
   return (
     <div>
       <h1>6 posts, then 1 thank you</h1>
+      {props.items.maps((item) => {
+        return (
+          <div key={item.id}>
+            <ul>
+              <li>
+                <img src={item.profilepic} alt="" />
+                {item.username}
+              </li>
+              <li>{item.numadults} adults</li>
+              <li>{item.numkids} kids</li>
+              <li>I need money for {item.reason}</li>
+              <li>I am a {item.jobclass}</li>
+              <li>
+                <button>Doante</button>
+                {item.payinfo}
+              </li>
+              <hr />
+              <li>
+                My story <button>v</button>
+              </li>
+              <li>{item.mystory}</li>
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
 };
